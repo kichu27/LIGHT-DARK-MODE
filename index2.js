eca32df898850036abf5e1aba03c9aaa7920dc32
Kartik Patekar <99249006+kichu27@users.noreply.github.com>
2022-09-11 21:35:02 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+
+$("#toggle").on('click', function () {
+$('body').css('background-color' , 'black') ;
+$('h1').css('color' , 'white') ;
+$('p').css('color' , 'white') ;
+
+})
+
+
+
+$("#toggle1").on('click', function () {
+$('body').css('background-color' , 'white') ;
+$('h1').css('color' , 'black') ;
+$('p').css('color' , 'black') ;
+})
+
+})
